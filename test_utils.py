ecba6f8297b188c81fe171eb1de39e2951ee283a
vijaygadhave199 <vijaygadhave199@gmail.com>
2025-07-18 14:05:16 +0530

    Update test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -1,4 +1,6 @@
 # test_utils.py
-# TODO: Write unit test for add_numbers()
+from utils import add_numbers
 def test_add_numbers():
-    pass
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(-1, 1) == 0
+    assert add_numbers(0, 0) == 0
